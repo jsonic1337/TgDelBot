aceca3bcc5a0e2fdf4941063ac1db9dc3ee73400
Oleg <50801614+jsonic1337@users.noreply.github.com>
2021-09-22 15:41:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+from pyrogram import Client, filters
+
+delall = "/delall"
+del_count = "ой"
+del_count_edit = "ой-"
+
+app = Client(
+    "my_account",
+    api_id=,    # Fill in from the https://my.telegram.org/apps
+    api_hash="" # Fill in from the https://my.telegram.org/apps
+)
+
+@app.on_message()
+def bot(client, message):
+    print(message)
+    if message["text"] is not None:
+        if message["from_user"]["is_self"] and message["text"] == delall:
+            chat_id = message["chat"]["id"]
+            msgs_list = Client.get_history(chat_id=chat_id, self=app)
+            ids_list = []
+            for i in msgs_list:
+                if i["chat"]["id"] == chat_id and i["from_user"]["is_self"]:
+                    ids_list.append(i["message_id"])
+            Client.delete_messages(chat_id=chat_id, message_ids=ids_list, self=app)
+
+        if message["from_user"]["is_self"] and message["text"].lower().startswith(del_count):
+            count = 0
+            if (message["text"])[2:].isdigit():
+                count = int((message["text"])[2:])+1
+            elif message["text"].lower() == del_count:
+                count = 2
+            if count > 0:
+                chat_id = message["chat"]["id"]
+                msgs_list = Client.get_history(chat_id=chat_id, self=app)
+                ids_list = []
+                for i in range(0, len(msgs_list)):
+                    if msgs_list[i]["chat"]["id"] == chat_id and msgs_list[i]["from_user"]["is_self"]:
+                        ids_list.append(msgs_list[i]["message_id"])
+                        if len(ids_list) == count: break
+                Client.delete_messages(chat_id=chat_id, message_ids=ids_list, self=app)
+
+        if message["from_user"]["is_self"] and message["text"].lower().startswith(del_count_edit):
+            count = 0
+            if (message["text"])[3:].isdigit():
+                count = int((message["text"])[3:])+1
+            elif message["text"].lower() == del_count_edit:
+                count = 2
+            if count > 0:
+                chat_id = message["chat"]["id"]
+                msgs_list = Client.get_history(chat_id=chat_id, self=app, limit=count)
+                ids_list = []
+                for i in range(0, len(msgs_list)):
+                    if msgs_list[i]["chat"]["id"] == chat_id and msgs_list[i]["from_user"]["is_self"]:
+                        ids_list.append(msgs_list[i]["message_id"])
+                        app.edit_message_text(chat_id=chat_id, message_id=msgs_list[i]["message_id"], text="[DELETED]")
+                        if len(ids_list) == count: break
+                Client.delete_messages(chat_id=chat_id, message_ids=ids_list, self=app)
+app.run()
+
+
+
