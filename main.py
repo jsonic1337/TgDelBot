aeffd994f5471d293e8480ea4f462af463498255
Oleg <50801614+jsonic1337@users.noreply.github.com>
2024-05-12 22:26:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,56 +6,31 @@ del_count_edit = "oh-"
 
 app = Client(
     "my_account",
-    api_id=,    # Fill in from the https://my.telegram.org/apps
+    api_id=123456,    # Fill in from the https://my.telegram.org/apps
     api_hash="" # Fill in from the https://my.telegram.org/apps
 )
 
-@app.on_message()
+@app.on_message(filters.text & filters.outgoing)
 def bot(client, message):
-    print(message)
-    if message["text"] is not None:
-        if message["from_user"]["is_self"] and message["text"] == delall:
-            chat_id = message["chat"]["id"]
-            msgs_list = Client.get_history(chat_id=chat_id, self=app)
-            ids_list = []
-            for i in msgs_list:
-                if i["chat"]["id"] == chat_id and i["from_user"]["is_self"]:
-                    ids_list.append(i["message_id"])
-            Client.delete_messages(chat_id=chat_id, message_ids=ids_list, self=app)
+    chat_id = message.chat.id
+    if message.text == delall:
+        msgs_list = client.get_chat_history(chat_id)
+        ids_list = [msg.id for msg in msgs_list if msg.from_user.is_self]
+        client.delete_messages(chat_id=chat_id, message_ids=ids_list)
 
-        if message["from_user"]["is_self"] and message["text"].lower().startswith(del_count):
-            count = 0
-            if (message["text"])[2:].isdigit():
-                count = int((message["text"])[2:])+1
-            elif message["text"].lower() == del_count:
-                count = 2
-            if count > 0:
-                chat_id = message["chat"]["id"]
-                msgs_list = Client.get_history(chat_id=chat_id, self=app)
-                ids_list = []
-                for i in range(0, len(msgs_list)):
-                    if msgs_list[i]["chat"]["id"] == chat_id and msgs_list[i]["from_user"]["is_self"]:
-                        ids_list.append(msgs_list[i]["message_id"])
-                        if len(ids_list) == count: break
-                Client.delete_messages(chat_id=chat_id, message_ids=ids_list, self=app)
+    elif message.text.lower().startswith(del_count) and not (message.text.lower().startswith(del_count_edit)):
+        count = 2 if message.text.lower() == del_count else int(message.text[2:])+1
+        msgs_list = client.get_chat_history(chat_id)
+        ids_list = [msg.id for msg in msgs_list if msg.from_user.is_self][:count]
+        client.delete_messages(chat_id=chat_id, message_ids=ids_list)
 
-        if message["from_user"]["is_self"] and message["text"].lower().startswith(del_count_edit):
-            count = 0
-            if (message["text"])[3:].isdigit():
-                count = int((message["text"])[3:])+1
-            elif message["text"].lower() == del_count_edit:
-                count = 2
-            if count > 0:
-                chat_id = message["chat"]["id"]
-                msgs_list = Client.get_history(chat_id=chat_id, self=app, limit=count)
-                ids_list = []
-                for i in range(0, len(msgs_list)):
-                    if msgs_list[i]["chat"]["id"] == chat_id and msgs_list[i]["from_user"]["is_self"]:
-                        ids_list.append(msgs_list[i]["message_id"])
-                        app.edit_message_text(chat_id=chat_id, message_id=msgs_list[i]["message_id"], text="[DELETED]")
-                        if len(ids_list) == count: break
-                Client.delete_messages(chat_id=chat_id, message_ids=ids_list, self=app)
+    elif message.text.lower().startswith(del_count_edit):
+        count = 2 if message.text.lower() == del_count_edit else int(message.text[3:])+1
+        msgs_list = client.get_chat_history(chat_id)
+        ids_list = [msg.id for msg in msgs_list if msg.from_user.is_self][:count]
+        for i in ids_list:
+            client.edit_message_text(chat_id=chat_id, message_id=i, text="[DELETED]")
+            print(i)
+        client.delete_messages(chat_id=chat_id, message_ids=ids_list)
+
 app.run()
-
-
-
